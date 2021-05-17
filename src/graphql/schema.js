d9772d0ce8b260c47f6e3741ca19b3634ef3f14b
Jude K <judekuti@gmail.com>
2021-05-17 16:06:18 -0500

    Attempt type system with GraphQL Schema

---
+++ src/graphql/schema.js
@@ -0,0 +1,107 @@
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLScalarType,
+  GraphQLInt,
+  GraphQLList,
+  buildSchema
+} from 'graphql';
+
+// import getData from '../http';
+
+const TextMatchHighlightType = new GraphQLObjectType({
+  name: 'TextMatchHighlightQuery',
+  fields: () => ({
+    beginIndice: { type: GraphQLInt },
+    endIndice: { type: GraphQLInt },
+    text: { type: GraphQLString }
+  })
+});
+
+const TextMatchType = new GraphQLObjectType({
+  name: 'TextMatchQuery',
+  fields: () => ({
+    fragment: { type: GraphQLString },
+    highlights: { type: [TextMatchHighlightType] },
+    property: { type: GraphQLString }
+  })
+});
+
+const SearchResultItemEdgeType = new GraphQLObjectType({
+  name: 'SearchResultItemEdgeQuery',
+  fields: () => ({
+    cursor: { type: GraphQLString },
+    node: { type: SearchResultItemEdgeType },
+    textMatches: { type: TextMatchType }
+  })
+});
+
+// Edge = [SearchResultItemEdge]
+
+// Test case
+const UserType = new GraphQLObjectType({
+  name: 'UserQuery',
+  fields: () => ({
+    login: { type: GraphQLString },
+    name: { type: GraphQLString },
+    location: { type: GraphQLString },
+    bio: { type: GraphQLString },
+  })
+});
+
+const me = `{
+  viewer{
+    name
+    login
+    location
+  }
+}`
+
+
+const TaskType = new GraphQLObjectType({
+  name: 'TaskQuery',
+  fields: () => ({
+    login: { type: GraphQLString },
+    name: { type: GraphQLString },
+    location: { type: GraphQLString },
+    bio: { type: GraphQLString },
+  })
+});
+
+
+const SearchType = new GraphQLObjectType({
+  name: 'SearchQuery',
+  fields: () => ({
+    search: { type: GraphQLString },
+  })
+})
+
+const ComboType = new GraphQLObjectType({
+  name: 'ComboQuery',
+  fields: () => ({
+
+ })
+})
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    viewer: {
+      type: UserType,
+      async resolve(parentValue, args) {
+        // do stuff here
+        // return await getData(me)
+
+      }
+    }
+  }
+});
+
+const schema = new GraphQLSchema({
+  query: RootQuery
+})
+
+// Logging as requested
+graphql(schema, me).then(x => console.log(JSON.stringify(x)))
+export default schema;
