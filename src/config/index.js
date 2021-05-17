57f424a5a640315aa449ee9c38f701957001adcf
Jude K <judekuti@gmail.com>
2021-05-17 16:02:15 -0500

    Create config to access user env variables

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+// Environment variables from the user environment.
+
+const commonConfig = {
+    env: process.env.NODE_ENV || 'development',
+    port: parseInt(process.env.PORT, 10) || 5000,
+    corsDomain: process.env.CORS_DOMAIN || '*',
+    token: process.env.TOKEN
+};
+
+export default commonConfig;
